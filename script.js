8b1e440c9bfe615e80aca87753af712bf5edf7dd
Joaquinn <joaov.b.euwest@gmail.com>
2025-06-17 20:52:08 -0300

    #Adicionando um segundo array como resposta

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-let piadas = ['Qual a velocidade do escuro? Depende da viatura', 'Você sabe como deixa um carteiro triste ? Você mata a família dele', 'Porque o padre não pode xingar na hora do sexo: Para não ensinar palavrão pra criança', "Um cara forte e um cara fraco assaltam uma mulher. Em quem você bate? 'Na mulher'" ];
+let piadas = ['Qual é a sua coisa favorita em mim?', 'Que tipo de aventura você gostaria de ter juntos?', 'Jantar romântico ou noite de cinema em casa?', "Você tem algum talento escondido?", 'Gostaria de tentar fantasias de casal para o Halloween?', 'Como é a sua casa dos sonhos?', 'Onde é um lugar que você sempre quis visitar?' ];
+
+let piadas02 = ['Como quiser', 'Apenas faça', 'Esta é uma pergunta muito boa', 'Tem certeza que quer saber?', 'Isso é certo como o inferno', 'Você já sabe a resposta'];
 
 function numAleatorio(max) {
   return Math.floor(Math.random() * max)
 }
 const selecionarPiadaAleatoria = () => {
   const indexAleatorio = numAleatorio(piadas.length);
-  return piadas[indexAleatorio]
+  const indexAleatorio02 = numAleatorio(piadas02.length)
+  return`Pergunta: ${piadas[indexAleatorio]} Resposta: ${piadas02[indexAleatorio02]}`
 }
 console.log(selecionarPiadaAleatoria())
