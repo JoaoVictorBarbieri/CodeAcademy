9c8d79a3a9d406e1da45f40dc125dd459d88eba3
Joaquinn <joaov.b.euwest@gmail.com>
2025-09-24 17:58:58 -0300

    fix

--- project-jamming/src/components/SearchResults/SearchResults.jsx
+++ project-jamming/src/components/SearchResults/SearchResults.jsx
@@ -1,4 +1,5 @@
 import './SearchResults.css';
+import { useState } from 'react';
 import TrackList from '../TrackList/TrackList';
 
 const SearchResults = () => {
@@ -6,8 +7,11 @@ const SearchResults = () => {
         { id: 1, name: 'Tiny Dancer', artist: 'Elton John', album: 'Madman Across The Water' },
         { id: 2, name: 'Tiny Dancer', artist: 'Tim McGraw', album: 'Love Story' },
         { id: 3, name: 'Tiny Dancer', artist: 'Rockabye Baby!', album: 'Lullaby Renditions of Elton John' },
-        { id: 4, name: 'Tiny Dancer - Live Album Version', artist: 'Ben Folds', album: 'Ben Folds Live' }
+        { id: 4, name: 'Tiny Dancer - Live Album Version', artist: 'Ben Folds', album: 'Ben Folds Live' },
+        { id: 5, name: 'Amiga da minha Mulher', artist: 'Seu Jorge', album: 'Músicas para Churrasco Vol.1' }
     ];
+
+    const [searchResults, setSearchResults] = useState(results)
 
   return (
     <div className="SearchResults">
